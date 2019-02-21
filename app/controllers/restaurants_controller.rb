7dc964fe85324645100a053d4965e53dfa3eb185
Michael Frantz <mjfrantz26@gmail.com>
2019-02-21 11:05:20 +0900

    Work in progress

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,35 @@
+class RestaurantsController < ApplicationController
+    before_action :set_action, only: [:show, :edit]
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else 
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  private 
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :category, :phone_number)
+  end
+
+  def set_action
+    @restaurant = Restaurant.find(params[:id])
+  end
+end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,8 @@
 class Review < ApplicationRecord
     belongs_to :restaurant
+    RATINGS = %w[0 1 2 3 4 5]
     validates :content, presence: true
-    validates :rating, inclusion: { in: [ 0, 1, 2, 3, 4, 5 ] }, presence: true, numericality: true
+    validates :rating, inclusion: { in: RATINGS }, presence: true, numericality: true
 end
+
+
